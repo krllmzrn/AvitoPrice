71297ca476d97a9cb9da2620f4e55f2558f462bd
Кирилл <156343597+krllmzrn@users.noreply.github.com>
2024-03-12 06:03:22 +0500

    Three commit

--- src/main/java/com/example/avito/controllers/BaselineMatrixController.java
+++ src/main/java/com/example/avito/controllers/BaselineMatrixController.java
@@ -3,21 +3,25 @@ package com.example.avito.controllers;
 import com.example.avito.models.BaselineMatrix;
 import com.example.avito.repository.BaselineMatrixRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BaselineMatrixController {
     @Autowired
     BaselineMatrixRepository baselineMatrixRepository;
-    @GetMapping("")
-    public String showBaseline(Model model){
-        List<BaselineMatrix> baseline = baselineMatrixRepository.findAll();
-        model.addAttribute("BaselineMatrix",baseline);
+    @GetMapping("/")
+    public String showBaseline(@RequestParam(defaultValue = "0") int page,
+                               @RequestParam(defaultValue = "20") int size,
+                               Model model) {
+        PageRequest pageable = PageRequest.of(page, size);
+        Page<BaselineMatrix> baselinePage = baselineMatrixRepository.findAll(pageable);
+        model.addAttribute("baselinePage", baselinePage);
         return "index";
+        }
     }
-}
+
